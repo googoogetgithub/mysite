166e7a93276f5caa72cae8b0cb029ae3d23c1337
googoogetgithub <googooget.github@gmail.com>
2018-11-21 14:34:23 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/fav.png" sizes="32x32" >
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit|Prompt" >
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <title>Find Your GD Home! </title>
+  </head>
+  <body>
+
+
+      <nav>
+        <div>
+          <h1><a href="#"><img src="images/logo.png" title="Company Logo!"></a><span>Find your GD Home!</span></div>
+        </div>
+        <ul>
+            <li><a href="">Home</a>
+              <div class="menu-sub">
+                Something wicked this way come
+              </div>
+
+            </li>
+            <li><a href="">Buy</a></li>
+            <li><a href="">Rent</a></li>
+            <li><a href="">Promotion</a></li>
+            <li><a href="">About</a></li>
+        </ul>
+      </nav>
+
+
+
+      <container>
+        <section>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            บัดซบจริงๆเลย<br /><br />
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            บัดซบจริงๆเลย<br /><br />
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            บัดซบจริงๆเลย<br /><br />
+        </section>
+      </container>
+  </body>
+
+
+  
+</html>
